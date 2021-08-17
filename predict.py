d77cc91525c52b750815a1f2da8beaca9fb9f6ea
Jule Thome <jule.thome@gmail.com>
2021-08-17 12:46:47 +0200

    Cleanup: add raw prediction output

--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@ from sklearn.metrics import classification_report,confusion_matrix
 Read test data and load baummethoden.pkl
 '''
 x_test = pd.read_csv("data/x_test.csv")
-y_test = pd.read_csv("data/y_test.csv")
+#y_test = pd.read_csv("data/y_test.csv")
 
 baummethoden = pickle.load(open("models/baummethoden.pkl", "rb"))
 
@@ -15,6 +15,9 @@ baummethoden = pickle.load(open("models/baummethoden.pkl", "rb"))
 Predictions
 '''
 prediction = baummethoden.predict(x_test)
+print("Prediction for test data: ")
+print(prediction)
+print("\n")
 
 # Show confusion matrix and classification report
 print("Confusion matrix")
